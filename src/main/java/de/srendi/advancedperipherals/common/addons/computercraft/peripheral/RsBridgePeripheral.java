98a687fb6cec42d18745dd750978288723f7020f
Rudy Gamberini <rudygamberini@gmail.com>
2023-03-31 15:55:50 -0400

    Update RsBridgePeripheral.java

--- src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/RsBridgePeripheral.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/RsBridgePeripheral.java
@@ -349,7 +349,6 @@ public class RsBridgePeripheral extends BasePeripheral<BlockEntityPeripheralOwne
     @LuaFunction(mainThread = true)
     public final MethodResult isItemCrafting(String item) {
         return MethodResult.of(true);
-        return MethodResult.of(true);
         if (!isConnected())
             return notConnected();
 
